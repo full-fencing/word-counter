bef262a8486a5bbd603459f86502fad23281b369
lava m <lavanymkanna@gmail.com>
2016-09-13 15:11:02 -0400

    logic in views completed

--- app/views.py
+++ app/views.py
@@ -2,12 +2,20 @@ from flask import render_template, request
 
 from app import app
 from app.forms import WordCount
+from collections import Counter
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     wordcount_form = WordCount(request.form)
     if request.method == "POST" and wordcount_form.validate():
-        user_input = wordcount_form.data
-        print(user_input)
+        user = wordcount_form.user_input.data
+        words = user.split(" ")
+        word_count=len(words)
+        character= list(user)
+        character_count=len(character)
+        character = [ x for x in character if x != " "]
+        (Counter(words).most_common(2))
+        (Counter(character).most_common(2))
+        
     return render_template("index.html", wordcount_form=wordcount_form)
