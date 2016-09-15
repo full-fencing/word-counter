ce99218871eb445b05922be82c079f874d50fe64
tmcothran <tmcothran@hotmail.com>
2016-09-15 14:32:31 -0400

    Restyled the address book; form showing up on addres book.

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,12 @@
-from wtforms import Form, TextAreaField, validators, ValidationError
+from wtforms import Form, DecimalField, TextAreaField, validators, ValidationError
 
 
 class WordCount(Form):
     user_input = TextAreaField(
         "Type some words.", validators=[validators.DataRequired()]
     )
+
+
+class NumberOfUsers(Form):
+    user_input = DecimalField("How many users do you want?",
+                              validators=[validators.DataRequired()])
