1ab3212035b023435bfc269cbf55bfdf4e7745b0
lava m <lavanymkanna@gmail.com>
2016-09-16 16:28:55 -0400

    scrapy started

---
+++ app/lib/scraper.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class CNNSpider (scrapy.Spider):
+    name = "CNN"
+
+    start_urls = ['http://www.cnn.com/']
+
+    def parse(self,response):
+
+        for href in response.css ('ul.cn--idx-1 a::attr(href)'):
+            full_url = response.urljoin(href.extract())
+            yield scrapy.Request(full_url, callback = self.parse_article)
+
+    def parse_article(self,response):
+
+        yield{
+        'title': response.css('h1.pg-headline::text').extract_first()
+        }
+
